1056b3b742da4cc039058c67f961dcfe8c87cb53
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-13 16:44:48 +0100

    generato il container inizio personalizzazione

--- js/main.js
+++ js/main.js
@@ -15,49 +15,49 @@ $(document).ready(function() {
   console.log(meseGennaio);
 
   console.log(meseGennaio.includes(1));
-    //chiamata per il mese di gennaio per scaricare festività
-  $.ajax({
-      url: "https://holidayapi.com/v1/holidays",
-      method: "GET",
-      data: {
-        key: "4c787bc0-88cd-4e5a-b4be-5e9f59b73ab7",
-        country: "IT",
-        year: 2017,
-        month: 1
-      },
-      success: function(data, stato) {
-        var infoMese = data;
-        console.log(infoMese);
-        console.log(infoMese.holidays.length);
-        for (var i = 0; i < infoMese.holidays.length; i++) {
-          //salvo le festevita del mese
-          var festivitaRecuperata = infoMese.holidays[i].date;
-          //console.log(festivitaRecuperata);
-          //poi le converto in un numero singolo
-          var dataFestivita = moment(festivitaRecuperata).format("D");
-          console.log("festa: " + dataFestivita);
-          //applico il ciclo while con all'interno il confronta giorno / festivita
-          var trovato = false ;
-          var contatore = 0;
-          while(!trovato){
-            if(meseGennaio[contatore] == dataFestivita) {
-              console.log("trovato");
-              trovato = true ;
-            }
-            else {
-              console.log("non trovato");
-            }
-
-            contatore ++;
+  //chiamata per il mese di gennaio per scaricare festività
+  /*$.ajax({
+    url: "https://holidayapi.com/v1/holidays",
+    method: "GET",
+    data: {
+      key: "4c787bc0-88cd-4e5a-b4be-5e9f59b73ab7",
+      country: "IT",
+      year: 2017,
+      month: 1
+    },
+    success: function(data, stato) {
+      var infoMese = data;
+      console.log(infoMese);
+      console.log(infoMese.holidays.length);
+      for (var i = 0; i < infoMese.holidays.length; i++) {
+        //salvo le festevita del mese
+        var festivitaRecuperata = infoMese.holidays[i].date;
+        //console.log(festivitaRecuperata);
+        //poi le converto in un numero singolo
+        var dataFestivita = moment(festivitaRecuperata).format("D");
+        console.log("festa: " + dataFestivita);
+        //applico il ciclo while con all'interno il confronta giorno / festivita
+        var trovato = false;
+        var contatore = 0;
+        while (!trovato) {
+          if (meseGennaio[contatore] == dataFestivita) {
+            console.log(contatore);
+            console.log("trovato");
+            trovato = true;
+          } else {
+            console.log("non trovato");
           }
+
+          contatore++;
         }
-
-
-      },
-      error: function(richiesta, stato, errori) {
-        console.log("c'è stato un errore " + errori);
       }
 
 
-  });
+    },
+    error: function(richiesta, stato, errori) {
+      console.log("c'è stato un errore " + errori);
+    }
+
+
+  });*/
 });
