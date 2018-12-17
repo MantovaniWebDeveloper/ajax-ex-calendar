9e170d5560a19001379c01590f90ebf4345d7108
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-12-17 10:38:11 +0100

    trasformazione in funzione stampaGiorni con cambio mese, ora bug su html

--- js/main.js
+++ js/main.js
@@ -1,64 +1,71 @@
 $(document).ready(function() {
   //creo data
-  var mese = moment('2017-01');
-  $('#nomeMese').html(mese.format('MMMM YYYY'));
-  //trovato il metedo di moment per estrapolare quanti giorni possiede un mese
-  var giorniGennaio = moment(mese, "YYYY-MM").daysInMonth();
-  console.log(giorniGennaio);
-  //salvo tutti i giorni di gennaio in un oggetto;
-  var gennaio = {
-    giorni: [],
-    nomeGiorni: [],
-    festivita: []
-  }
-  var giorno = 0;
-  var giornoMese;
-  for (var i = 1; i <= giorniGennaio; i++) {
-    giorno = i;
-    gennaio.giorni.push(giorno);
-    giornoMese = moment("2017-01-" + giorno).format("ddd");
-    console.log(giornoMese)
-    gennaio.nomeGiorni.push(giornoMese);
-  }
-  console.log(gennaio);
-
-  for (var i = 0; i < gennaio.giorni.length; i++) {
-    var templateBase = $('#listaGiorni').html();
-    var templateCompilato = Handlebars.compile(templateBase);
-    var context = {
-      giorni: gennaio.giorni[i],
-      nomiGiorniMese: gennaio.nomeGiorni[i]
-    };
-    var htmlStampato = templateCompilato(context);
-    $('#wrapElenco').append(htmlStampato);
-  }
-  //Al click del pulsante seleziona
-  $('#conferma').click(function() {
-    //recupero il valore dalla select
-    var nazione = $('#selectListaNazioni').val();
-    console.log("nazione scelta " + nazione);
-    chimataApi(nazione,mese);
-  });
+  var date = moment('2017-01');
+  stampaGiorni(date);
+  console.log("prima date" + date);
   //AL click del pulsante Prossimo
   $('#prossimo').click(function() {
     //alert("cliccato");
     //uso il metodo per aggiungere una data
-    mese.add(1,'M');
-    $('#nomeMese').html(mese.format('MMMM YYYY'));
+    date.add(1,'M');
+    $('#nomeMese').html(date.format('MMMM YYYY'));
+    stampaGiorni(date);
 
   });
   //AL click del pulsante Prossimo
   $('#precedente').click(function() {
     //alert("cliccato");
     //uso il metodo per sottrarre una data
-    mese.subtract(1,'M');
-    $('#nomeMese').html(mese.format('MMMM YYYY'));
+    date.subtract(1,'M');
+    $('#nomeMese').html(date.format('MMMM YYYY'));
+    stampaGiorni(date);
+  });
+  function stampaGiorni(date){
+    $('#nomeMese').html(date.format('MMMM YYYY'));
+    //trovato il metedo di moment per estrapolare quanti giorni possiede un mese
 
+    var giorniMese = moment(date, "YYYY-MM").daysInMonth();
+    console.log(giorniMese);
+    //salvo tutti i giorni di gennaio in un oggetto;
+    var mese = {
+      giorni: [],
+      nomeGiorni: [],
+      festivita: []
+    }
+    var giorno = 0;
+    var giornoMese;
+    for (var i = 1; i <= giorniMese; i++) {
+      giorno = i;
+      mese.giorni.push(giorno);
+      giornoMese = moment("2017-01-" + giorno).format("ddd");
+      console.log(giornoMese)
+      mese.nomeGiorni.push(giornoMese);
+    }
+
+    for (var i = 0; i < mese.giorni.length; i++) {
+      var templateBase = $('#listaGiorni').html();
+      var templateCompilato = Handlebars.compile(templateBase);
+      var context = {
+        giorni: mese.giorni[i],
+        nomiGiorniMese: mese.nomeGiorni[i]
+      };
+      var htmlStampato = templateCompilato(context);
+      $('#wrapElenco').append(htmlStampato);
+
+    }
+  }
+  //Al click del pulsante seleziona
+  $('#conferma').click(function() {
+    //recupero il valore dalla select
+    var nazione = $('#selectListaNazioni').val();
+    console.log("nazione scelta " + nazione);
+    chimataApi(nazione,date);
   });
 
-  function chimataApi(nazione,mese) {
+
+  function chimataApi(nazione,date) {
     //chiamata per il mese di gennaio per scaricare festività
-   $.ajax({
+  /* $.ajax({
       url: "https://holidayapi.com/v1/holidays",
       method: "GET",
       data: {
@@ -93,7 +100,7 @@ $(document).ready(function() {
       }
 
 
-    });
+    });*/
   }
 
 });
